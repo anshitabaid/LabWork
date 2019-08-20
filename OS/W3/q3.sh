b53d813f3be0f08a3028bedf61f73859c878ca61
anshitabaid <anshita_b@yahoo.co.in>
2019-08-20 23:21:14 +0530

    Fixed OS lab 3

--- OS/W3/q3.sh
+++ OS/W3/q3.sh
@@ -2,33 +2,37 @@ echo "Enter  a b and c"
 read a
 read b
 read c
-d=`expr $b \* $b - 4 \* $a \* $c`
+d=$(((b*b)- 4*a*c))
 if [ $d == 0 ] 
 then d1=0
+elif [ $d -lt 0 ]
+	then d1=-1
 else
-d1=`expr $d / $d`
+	d1=1
 fi
-echo $d1
 r1=0
 r2=0
 case $d1 in
 "0")
 	echo "roots equal"
-	r1=`expr -1 \* $b / \( 2 \* $a \)`
+	r1=$(( -b / 2*a))
+	#r1=`expr -1 \* $b / \( 2 \* $a \)`
 	r2=$r1
 	;;
 "1")
 	echo "roots real and distinct"
-	d=`expr sqrt \( $d \) | bc`
-	r1=`expr \( -1 \* $b + $d \) / \( $2 \* $a \)`
-	r2=`expr \( -1 \* $b - $d \) / \( $2 \* $a \)`
+	d=$(echo "sqrt ($d)" | bc)
+	r1=$(((-b + d) / (2*a)))
+	r2=$(((-b - d) / (2*a)))
+	#r1=`expr \( -1 \* $b + $d \) / \( 2 \* $a \)`
+	#r2=`expr \( -1 \* $b - $d \) / \( 2 \* $a \)`
 	;;
 "-1")
 	echo "roots imaginary"
-	d= `expr $d \* - 1`
-	d=`expr sqrt \( $d \)`
-	r1 = `expr -1 \*  $b / \( 2 \* $a \)`
-	r2 = `expr $d / \( 2 \* $a \)`
+	d=$((-1 * d))
+	d=$(echo "sqrt ($d)" | bc)
+	r1=`expr -1 \*  $b / \( 2 \* $a \)`
+	r2=`expr $d / \( 2 \* $a \)`
 	;;
 *)
 	echo "nothing"
